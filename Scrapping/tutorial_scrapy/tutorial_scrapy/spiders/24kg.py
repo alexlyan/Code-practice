ce051adc035d0e1efed55a3585b9747061b4d9f0
Alex <alexlyan@yahoo.com>
2020-06-20 13:00:05 +0600

    crawler for 24.kg

--- Scrapping/tutorial_scrapy/tutorial_scrapy/spiders/24kg.py
+++ Scrapping/tutorial_scrapy/tutorial_scrapy/spiders/24kg.py
@@ -2,6 +2,7 @@ import scrapy
 from datetime import date
 import time
 from datetime import datetime
+import re
 
 class QuotesSpider(scrapy.Spider):
     name = "24"
@@ -18,10 +19,10 @@ class QuotesSpider(scrapy.Spider):
     def get_links(self, response):
         links = []
 
-        for link in response.xpath('/html/body/div[2]/div[6]/div/div/div[1]/ul/li[2]/ul/li'):
-            time.sleep(2)
+        for link in response.xpath('/html/body/div[4]/div[5]/div[1]/div/div/div[2]/div/div[2]/a/@href')[:10]:
+            time.sleep(1)
 
-            href = link.xpath('div/a/@href').get()
+            href = 'https://24.kg'+link.get()
             links.append(href)
         
         for link in links:
@@ -30,17 +31,48 @@ class QuotesSpider(scrapy.Spider):
     # main function for extracting data
     def parse(self, response):
 
-        paragraphs = [] 
-        for paragraph in response.xpath('/html/body/div[2]/div[5]/div/div/div/div[4]/div[4]/p/text()'):  
-            paragraphs.append(paragraph.get())
+        # part for parsing paragraphs
+        paragraphs = []
+        
+        # another pattern '//*[@id="content-wrapper"]/div[1]/div[1]/div[4]/p'
+        
+        if response.xpath('//*[@id="content-wrapper"]/div[1]/div[1]/div[3]/p'):
+            xpath_paragraph = response.xpath('//*[@id="content-wrapper"]/div[1]/div[1]/div[3]/p')
+        elif response.xpath('//*[@id="content-wrapper"]/div[1]/div[1]/div[4]/p'):
+            xpath_paragraph = response.xpath('//*[@id="content-wrapper"]/div[1]/div[1]/div[4]/p')
+        else:
+            xpath_paragraph = response.xpath('//*[@id="content-wrapper"]/div[1]/div[1]/div[3]/div[2]/p')
 
+        # get numbers of paragraphs
+        len_pars = len(xpath_paragraph)
+
+        for par in range(len_pars):
+            # find all patterns and join with ' ' and replace all bytecode parts to space
+            paragraph = ' '.join(re.findall(r'>(.*?)<', xpath_paragraph[par].get())).replace('\xa0', ' ')
+            paragraphs.append(paragraph)
+        
+        # parsing time 
         time.sleep(1)
 
+        # parsing time
+        if response.xpath('//*[@id="content-wrapper"]/div[1]/div[1]/div[3]/div/span[1]/@content'):
+            time_date = datetime.strptime(response.xpath('//*[@id="content-wrapper"]/div[1]/div[1]/div[3]/div/span[1]/@content').extract()[0][:10], '%Y-%m-%d')
+        else:
+            time_date = datetime.strptime(response.xpath('//*[@id="content-wrapper"]/div[1]/div[1]/div[1]/div[2]/span[1]/@content').extract()[0][:10], '%Y-%m-%d')
+
+
+        # parsing view number
+
+        if response.xpath('/html/body/div[4]/div[2]/div[1]/div[1]/div[5]/div[1]/text()[4]').get():
+            views = int(re.findall(r'(\d+)', response.xpath('/html/body/div[4]/div[2]/div[1]/div[1]/div[5]/div[1]/text()[4]').get())[0])
+        else:
+            views = int(re.findall(r'(\d+)', response.xpath('/html/body/div[4]/div[2]/div[1]/div[1]/div[4]/div[1]/text()[4]').get())[0])
+
         yield {
-            'time': datetime.strptime(response.xpath('/html/body/div[2]/div[5]/div/div/div/div/div[2]/time/@datetime').extract()[0],'%Y-%m-%dT%H:%M:%S%z'),
-            'topic': response.xpath('/html/body/div[2]/div[5]/div/div/div/p/a/span/text()').get(),
-            'header': response.xpath('/html/body/div[2]/div[5]/div/div/div/h1/span[1]/text()').get(),
+            'time': time_date,
+            'url': response.url,
+            'topic': response.xpath('//*[@id="content-wrapper"]/div[1]/div[1]/div[1]/div[1]/a/text()').get().strip(),
+            'header': response.xpath('//*[@id="content-wrapper"]/div[1]/div[1]/h1/text()').get().replace('\xa0', ' '),
             'text': ' '.join(paragraphs),
-            'views': response.xpath("/html/body/div[2]/div[5]/div/div/div/div/div[1]/span/text()").get().strip(), ###
-            'comment': response.xpath("/html/body/div[2]/div[5]/div/div/div/div/div[1]/span[2]/text()").get().strip()
+            'views': views
         }
